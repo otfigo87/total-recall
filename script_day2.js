68e6b38eb3c9648561260a2529669cf8be2c3e05
Otmane Aatik <aaotmane@gmail.com>
2023-02-15 09:47:14 -0500

    FizzBuzz answered

--- script_day2.js
+++ script_day2.js
@@ -10,11 +10,20 @@
 // }
 
 // !Fizz Buzz
+// for( let i = 1; i <= 100; i++){
+//     if(i%3 === 0 && i%5 === 0){
+//         console.log(`FizzBuzz`)
+//     }else if (i%5 === 0){
+//         console.log(`Fizz`)
+//     }else if (i%3 === 0){
+//         console.log(`Buzz`)
+//     }else{
+//         console.log(i)
+//     }
+// }
 
 // !Wild Wild Life
-////////////////////////////////
 
-////////////////////////////////
 // !Yell at the Ninja Turtles
 ////////////////////////////////
 
