f7b7d488eead9af979d9ea3523e3f4f3b3001489
Otmane Aatik <aaotmane@gmail.com>
2023-02-15 21:31:18 -0500

    Extra Cat Combinator

--- script_day1.js
+++ script_day1.js
@@ -385,6 +385,39 @@
 //_____________________________________________________________
 
 
-// Extra
+// !Extra
+// 1.
+const cat1 = {
+    name: "Kitty",
+    breed: "idk",
+    age: 2
+}
+console.log(cat1.age)
+console.log(cat1.breed)
+
+//2
+const cat2 = {
+    name: "brezz",
+    breed: "idk",
+    age: 3
+}
+
+//3
+const combineCats = (mama, papa) => {
+      return   {
+        name: mama.name+papa.name,
+        age: 1,
+        breed: `${mama.breed}-${papa.breed}`
+    }
+}
+console.log(combineCats(cat1, cat2));
+
+//4
+console.log(combineCats(
+  combineCats(combineCats(cat1, cat2), combineCats(cat1, cat2)),
+  combineCats(combineCats(cat1, cat2), combineCats(cat1, cat2))
+));
+
+
 
 
